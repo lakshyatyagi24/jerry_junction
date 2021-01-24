0f4a1afaceaa823b3a8bfbebb9132c3c33d651ad
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-01-25 00:25:37 +0530

    images

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,31 +1,100 @@
 import product1 from '../../images/1.jpg';
+import product2 from '../../images/2.jpg';
+import product3 from '../../images/3.jpg';
+import product4 from '../../images/4.jpeg';
+import product5 from '../../images/5.jpg';
+import product6 from '../../images/6.jpg';
+import product7 from '../../images/7.jpeg';
+import product8 from '../../images/8.jpeg';
+import product9 from '../../images/9.jpg';
+import product10 from '../../images/10.jpg';
+import product11 from '../../images/11.jpg';
+import product12 from '../../images/12.jpg';
+import product13 from '../../images/13.jpg';
 
 export const productData = [
   {
-    img: product1,
+    img: product2,
     alt: 'Dahi Ke Sholey',
     name: 'Dahi Ke Sholey',
-    desc:
-      'Dahi Ke Sholey',
     price: 'Rs. 99',
     button: 'Add to Cart'
   },
   {
-    img: product1,
-    alt: 'Paneer Tikka',
-    name: 'Paneer Tikka',
-    desc:
-      'Paneer Tikka',
+    img: product3,
+    alt: 'Paneer Tikka ',
+    name: 'Paneer Tikka ',
       price: 'Rs. 99',
       button: 'Add to Cart'
   },
   {
-    img: product1,
+    img: product4,
     alt: 'Achari Paneer Tikka',
     name: 'Achari Paneer Tikka',
-    desc:
-      'Achari Paneer Tikka',
       price: 'Rs. 99',
+      button: 'Add to Cart'
+  },
+  {
+    img: product5,
+    alt: 'Veg Seekh Kabab',
+    name: 'Veg Seekh Kabab',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product6,
+    alt: 'Mushroom Tikka',
+    name: 'Mushroom Tikka',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product7,
+    alt: 'Afgani Chaap',
+    name: 'Afgani Chaap',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product8,
+    alt: 'Haryali Paneer',
+    name: 'Haryali Paneer',
+      price: 'Rs. 99',
+      button: 'Add to Cart'
+  },
+  {
+    img: product9,
+    alt: 'Haryali Chaap',
+    name: 'Haryali Chaap',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product10,
+    alt: 'Malai Chaap',
+    name: 'Malai Chaap',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product11,
+    alt: 'Khatta Aloo',
+    name: 'Khatta Aloo',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product12,
+    alt: 'Special Bhatti Bharwan Kumbh',
+    name: 'Special Bhatti Bharwan Kumbh',
+      price: 'Rs. 79',
+      button: 'Add to Cart'
+  },
+  {
+    img: product13,
+    alt: 'Veg Platter',
+    name: 'Veg Platter',
+      price: 'Rs. 79',
       button: 'Add to Cart'
   }
 ];
