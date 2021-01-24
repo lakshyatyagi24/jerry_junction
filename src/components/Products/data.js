5474cdd64738d55700259bbd7bbcfcebe562263d
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-01-24 22:53:25 +0530

    products created

---
+++ src/components/Products/data.js
@@ -0,0 +1,61 @@
+import product1 from '../../images/1.jpeg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Biryani',
+    name: 'Biryani 500g',
+    desc:
+      'Biryani',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product1,
+    alt: 'Biryani',
+    name: 'Biryani 500g',
+    desc:
+      'Biryani',
+      price: '$19.99',
+      button: 'Add to Cart'
+  },
+  {
+    img: product1,
+    alt: 'Biryani',
+    name: 'Biryani 500g',
+    desc:
+      'Biryani',
+      price: '$19.99',
+      button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: product1,
+    alt: 'Biryani',
+    name: 'Biryani 500g',
+    desc:
+      'Biryani',
+      price: '$19.99',
+      button: 'Add to Cart'
+  },
+  {
+    img: product1,
+    alt: 'Biryani',
+    name: 'Biryani 500g',
+    desc:
+      'Biryani',
+      price: '$19.99',
+      button: 'Add to Cart'
+  },
+  {
+    img: product1,
+    alt: 'Biryani',
+    name: 'Biryani 500g',
+    desc:
+      'Biryani',
+      price: '$19.99',
+      button: 'Add to Cart'
+  }
+];
