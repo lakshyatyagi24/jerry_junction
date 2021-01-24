1be9f455f13e099e4af33187033760361d344e70
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-01-24 23:03:30 +0530

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import { SidebarContainer, Icon, CloseIcon, SidebarMenu, SidebarLink, SidebarRoute, SideBtnWrap } from "./SidebarElements";
+import React from 'react';
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap
+} from './SidebarElements';
 
-const Sidebar = () => {
-    return (
-        <SidebarContainer>
-            <Icon>
-                <CloseIcon />
-            </Icon>
-            <SidebarMenu>
-                <SidebarLink to="/">Jerry's Junction</SidebarLink>
-                <SidebarLink to="/">Main Course</SidebarLink>
-                <SidebarLink to="/">Fast Food</SidebarLink>
-            </SidebarMenu>
-            <SideBtnWrap>
-                <SidebarRoute to='/'>Order Now</SidebarRoute>
-            </SideBtnWrap>
-        </SidebarContainer>
-    );
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarMenu>
+        <SidebarLink to='/'>Jerry's Junction</SidebarLink>
+        <SidebarLink to='/'>Main Course</SidebarLink>
+        <SidebarLink to='/'>Fast Food</SidebarLink>
+      </SidebarMenu>
+      <SideBtnWrap>
+        <SidebarRoute to='/'>Order Now</SidebarRoute>
+      </SideBtnWrap>
+    </SidebarContainer>
+  );
 };
 
 export default Sidebar;
+
